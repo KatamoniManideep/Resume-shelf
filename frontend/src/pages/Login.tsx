e94ec1ede26f276cdd1b1e18704384540d094a13
Katamoni Manideep <katamonimanideep04@gmail.com>
2026-06-17 12:58:39 +0530

    commit

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+
 import { useAuth } from '../context/AuthContext';
 import { Briefcase } from 'lucide-react';
 import api from "../api/axios";
